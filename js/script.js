321fb1597c5b1c5b99ffd4cbf4f966f2e7410c4b
Anastasiia Bondar <nastya.v.bondar@gmail.com>
2016-10-27 00:24:22 +0300

    Add language  version

--- js/script.js
+++ js/script.js
@@ -1,12 +1,23 @@
 $(document).ready(function() {
 
-        $('.hover-screen').mouseenter(function() {
-            var o = $(this).data('screen');
-            $('.screen').filter(o).addClass('active');
-        });
-
-        $('.hover-screen').mouseleave(function() {
-            var o = $(this).data('screen');
-            $('.screen').filter(o).removeClass('active');
-        });
+    $('.hover-screen').mouseenter(function() {
+        var o = $(this).data('screen');
+        $('.screen').filter(o).addClass('active');
     });
+
+    $('.hover-screen').mouseleave(function() {
+        var o = $(this).data('screen');
+        $('.screen').filter(o).removeClass('active');
+    });
+
+    $('.btn.lang').click(function() {
+        var $el = $(this);
+        var lang = $el.data('lang');
+
+        $("[data-localize]").localize("dict/dict", { language: lang });
+        $el.addClass("active");
+        $el.siblings().removeClass('active');
+    });
+
+
+});
